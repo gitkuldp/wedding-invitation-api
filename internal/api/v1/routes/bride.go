a6f7f658d4b149d81374d24d34045e527ee083af
Kebib <lamichhane.kebib@gmail.com>
2023-09-26 20:55:19 +0545

    minor changes at bride routes

--- internal/api/v1/routes/bride.go
+++ internal/api/v1/routes/bride.go
@@ -10,8 +10,8 @@ import (
 func BrideRoute(db *gorm.DB, group *echo.Group) {
 	brideService := services.NewBrideService(db)
 	ih := v1.NewBrideHandler(brideService)
-	group.POST("/bride", ih.Create)
-	group.GET("/bride", ih.List)
-	group.POST("/bride/:id", ih.Update)
-	group.GET("/bride/:id", ih.Find)
+	group.POST("/brides", ih.Create)
+	group.GET("/brides", ih.List)
+	group.POST("/brides/:id", ih.Update)
+	group.GET("/brides/:id", ih.Find)
 }
