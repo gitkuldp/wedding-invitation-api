67ef045242df7631ae7be1da7098ef2150fd6a8d
Kebib <lamichhane.kebib@gmail.com>
2023-09-26 20:55:42 +0545

    commented authroute for a while

--- internal/api/v1/routes/routes.go
+++ internal/api/v1/routes/routes.go
@@ -1,7 +1,6 @@
 package v1
 
 import (
-	"github.com/gitkuldp/wedding-invitation-api/internal/api/middleware"
 	"github.com/gitkuldp/wedding-invitation-api/internal/db"
 
 	"github.com/labstack/echo/v4"
@@ -9,10 +8,10 @@ import (
 
 func Setup(app *db.Application, echo *echo.Echo) {
 	erp := echo.Group("api/v1/erp")
-	authGroup := echo.Group("api/v1/erp")
+	// authGroup := echo.Group("api/v1/erp")
 
-	authGroup.Use(middleware.Authentication(app.Db))
-	RegisterUserRoute(app.Db, authGroup)
+	// authGroup.Use(middleware.Authentication(app.Db))
+	RegisterUserRoute(app.Db, erp)
 	LoginUserRoute(app.Db, erp)
 	RegisterInvitationRoute(app.Db, erp)
 	BrideRoute(app.Db, erp)
