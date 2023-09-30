f5ce64f5eaa0c659de11f696477e5ba9693f8823
gitkuldp <145738625+gitkuldp@users.noreply.github.com>
2023-09-30 10:46:07 +0545

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -39,6 +39,6 @@ func main() {
 	echo.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	v1.Setup(&app, echo)
-	err := echo.Start("https://wedding-invitation-api-production.up.railway.app/" + env.ServerAddress )
+	err := echo.Start("https://" + env.ServerAddress )
 	logrus.Error(err)
 }
